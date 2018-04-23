fe63bc851f556a75df2c22e245c232168eb20bb2
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomAxisLabels/Form1.Designer.vb
@@ -0,0 +1,101 @@
+Imports Microsoft.VisualBasic
+Imports System
+Namespace CustomAxisLabels
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+			Dim series1 As New DevExpress.XtraCharts.Series()
+			Dim pointSeriesLabel1 As New DevExpress.XtraCharts.PointSeriesLabel()
+			Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("A", New Object() { (CObj(-12))})
+			Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("B", New Object() { (CObj(-2))})
+			Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("C", New Object() { (CObj(3))})
+			Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("D", New Object() { (CObj(8))})
+			Dim lineSeriesView1 As New DevExpress.XtraCharts.LineSeriesView()
+			Dim pointSeriesLabel2 As New DevExpress.XtraCharts.PointSeriesLabel()
+			Dim lineSeriesView2 As New DevExpress.XtraCharts.LineSeriesView()
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(lineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(lineSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' chartControl1
+			' 
+			xyDiagram1.AxisX.Label.Visible = False
+			xyDiagram1.AxisX.Range.ScrollingRange.SideMarginsEnabled = True
+			xyDiagram1.AxisX.Range.SideMarginsEnabled = True
+			xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+			xyDiagram1.AxisY.Label.Font = New System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold)
+			xyDiagram1.AxisY.Range.ScrollingRange.SideMarginsEnabled = True
+			xyDiagram1.AxisY.Range.SideMarginsEnabled = True
+			xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+			Me.chartControl1.Diagram = xyDiagram1
+			Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.chartControl1.Legend.Visible = False
+			Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+			Me.chartControl1.Name = "chartControl1"
+			pointSeriesLabel1.LineVisible = True
+			pointSeriesLabel1.Visible = False
+			series1.Label = pointSeriesLabel1
+			series1.Name = "Series 1"
+			series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
+			series1.View = lineSeriesView1
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+			pointSeriesLabel2.LineVisible = True
+			Me.chartControl1.SeriesTemplate.Label = pointSeriesLabel2
+			Me.chartControl1.SeriesTemplate.View = lineSeriesView2
+			Me.chartControl1.Size = New System.Drawing.Size(261, 264)
+			Me.chartControl1.TabIndex = 0
+'			Me.chartControl1.CustomDrawAxisLabel += New DevExpress.XtraCharts.CustomDrawAxisLabelEventHandler(Me.chartControl1_CustomDrawAxisLabel);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(261, 264)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(lineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(lineSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
+	End Class
+End Namespace
+
