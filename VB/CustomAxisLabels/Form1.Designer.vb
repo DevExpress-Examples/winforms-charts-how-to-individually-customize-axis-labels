a74c13ef9aa870619aa19fedd8c4f3edc6cdc019
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomAxisLabels/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿Namespace CustomAxisLabels
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+            Dim series1 As New DevExpress.XtraCharts.Series()
+            Dim pointSeriesLabel1 As New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("A", New Object() { (DirectCast(-12R, Object))})
+            Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("B", New Object() { (DirectCast(-2R, Object))})
+            Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("C", New Object() { (DirectCast(3R, Object))})
+            Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("D", New Object() { (DirectCast(8R, Object))})
+            Dim lineSeriesView1 As New DevExpress.XtraCharts.LineSeriesView()
+            Dim pointSeriesLabel2 As New DevExpress.XtraCharts.PointSeriesLabel()
+            Dim lineSeriesView2 As New DevExpress.XtraCharts.LineSeriesView()
+            Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(lineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+            DirectCast(lineSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' chartControl1
+            ' 
+            xyDiagram1.AxisX.Label.Visible = False
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+            xyDiagram1.AxisY.Label.Font = New System.Drawing.Font("Tahoma", 8F, System.Drawing.FontStyle.Bold)
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+            Me.chartControl1.Diagram = xyDiagram1
+            Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False
+            Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+            Me.chartControl1.Name = "chartControl1"
+            pointSeriesLabel1.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            series1.Label = pointSeriesLabel1
+            series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False
+            series1.Name = "Series 1"
+            series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4})
+            series1.View = lineSeriesView1
+            Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+            pointSeriesLabel2.LineVisibility = DevExpress.Utils.DefaultBoolean.True
+            Me.chartControl1.SeriesTemplate.Label = pointSeriesLabel2
+            Me.chartControl1.SeriesTemplate.View = lineSeriesView2
+            Me.chartControl1.Size = New System.Drawing.Size(478, 287)
+            Me.chartControl1.TabIndex = 0
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(478, 287)
+            Me.Controls.Add(Me.chartControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            DirectCast(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(pointSeriesLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(lineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(series1, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(pointSeriesLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(lineSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
+            DirectCast(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
+    End Class
+End Namespace
+
---
+++ VB/CustomAxisLabels/Form1.vb
@@ -0,0 +1,29 @@
+﻿Imports System
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.XtraCharts
+
+Namespace CustomAxisLabels
+    Partial Public Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub chartControl1_CustomDrawAxisLabel(ByVal sender As Object, ByVal e As CustomDrawAxisLabelEventArgs) Handles chartControl1.CustomDrawAxisLabel
+            Dim axis As AxisBase = e.Item.Axis
+            If TypeOf axis Is AxisY OrElse TypeOf axis Is AxisY3D OrElse TypeOf axis Is RadarAxisY Then
+                Dim axisValue As Double = CDbl(e.Item.AxisValue)
+                If axisValue < 0 Then
+                    e.Item.TextColor = Color.Red
+                ElseIf axisValue > 0 Then
+                    e.Item.Text = "+" & e.Item.Text
+                    e.Item.TextColor = Color.Green
+                ElseIf axisValue = 0 Then
+                    e.Item.Text = "Zero"
+                End If
+            End If
+        End Sub
+    End Class
+End Namespace
